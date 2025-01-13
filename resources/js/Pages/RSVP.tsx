7f0fb5e2d730b69be3eea370ce8d7a87664453b5
Lei Nelissen <lei@codified.nl>
2025-01-13 22:22:20 +0100

    feat: refactor form to shadcn

--- resources/js/Pages/RSVP.tsx
+++ resources/js/Pages/RSVP.tsx
@@ -1,3 +1,7 @@
+import { Button } from '@/Components/ui/button';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/Components/ui/form';
+import { Input } from '@/Components/ui/input';
+import { Label } from '@/components/ui/label';
 import { AxiosError } from 'axios';
 import { shake } from 'radash';
 import { useCallback } from 'react';
@@ -14,13 +18,15 @@ interface Response {
 }
 
 export default function RSVP({ response }: RSVPProps) {
+    const form = useForm<Response>({ values: response || undefined});
     const {
         register,
         handleSubmit,
         watch,
         setError,
-        formState: { errors }
-    } = useForm<Response>({ values: response || undefined});
+        formState: { errors },
+        control
+    } = form;
     const persons = watch('persons', 0);
 
     const onSubmit = useCallback(async (data: Response) => {
@@ -31,8 +37,8 @@ export default function RSVP({ response }: RSVPProps) {
             if (e instanceof AxiosError) {
                 setError('root', { message: e.response?.data.message });
                 Object.keys(e.response?.data.errors).forEach((formKey) => {
-                    e.response?.data.errors[formKey].forEach((message) => {
-                        setError(formKey, { message });
+                    e.response?.data.errors[formKey].forEach((message: string) => {
+                        setError(formKey as keyof Response, { message });
                     });
                 })
             }
@@ -40,32 +46,64 @@ export default function RSVP({ response }: RSVPProps) {
     }, []);
 
     return (
-        <div className="max-w-[400px] p-8 mx-auto">
-            <form
-                onSubmit={handleSubmit(onSubmit)}
-                className="flex flex-col gap-4"
-            >
-                <label className="flex flex-col">
-                    <span>Name</span>
-                    <input type="text" {...register('name')} />
-                    {errors.name && (
-                        <span>{errors.name.message}</span>
-                    )}
-                </label>
-                <label className="flex flex-col">
-                    <span>№ of persons</span>
-                    <input type="number" {...register('persons')} placeholder="1" />
-                </label>
-                <label className="flex flex-col">
-                    <span>Email (if you'd like to receive updates)</span>
-                    <input type="text" {...register('email')} />
-                </label>
-                <button type="submit">
-                    {(persons || 0) > 1 ? 'We\'ll' : 'I\'ll'}
-                    {' '}
-                    be there!
-                </button>
-            </form>
+        <div className="max-w-[480px] p-8 mx-auto">
+            <Form {...form}>
+                <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className="flex flex-col gap-4"
+                >
+                    <FormField
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Your Name</FormLabel>
+                                <FormControl>
+                                    <Input type="text" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    Your given name or that of your organisation
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        name="persons"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>№ of persons</FormLabel>
+                                <FormControl>
+                                    <Input type="number" {...field} placeholder="1" />
+                                </FormControl>
+                                <FormDescription>
+                                    The size of your party. Don't forget to count yourself!
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input type="text" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    In case you'd like to receive an update e-mail with all details
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit">
+                        {(persons || 0) > 1 ? 'We\'ll' : 'I\'ll'}
+                        {' '}
+                        be there!
+                    </Button>
+                </form>
+            </Form>
         </div>
     )
 }
